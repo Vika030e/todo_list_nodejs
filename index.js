6030ca3cbde9abf8cd21e11a45e4e828b2f689c9
Vika030e <viktoriaradisuk44@gmail.com>
2024-03-16 15:25:01 +0200

    Added index.ejs file

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 var express = require('express');
 //Викликаємо express
 var app = express();
+//Налаштовуємо обробник шаблонів
+app.set("view engine", "ejs");
 //Налаштовуємо маршрут root(/) URL-адреси
 app.get('/', function (req, res) {
 //При відвідуванні root(/) URL-адреси у відповідь 
 //отримаємо повідомлення "Привіт, мій друже!"
- res.send('Привіт, мій друже!');
+ res.send('index');
 });
 //Налаштовуємо сервер для прослуховування порту 3000
 app.listen(3000, function() {
